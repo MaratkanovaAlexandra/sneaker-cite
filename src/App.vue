1798e5f11e2c05cb2432e9e2cf543536183065a5
Maratkanova Alexandra <alexandra.maratkanova@bravedevelopers.com>
2022-07-06 14:59:20 +0400

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-
+import { RouterView } from "vue-router";
+import AppHeader from "./components/AppHeader.vue";
 </script>
 
 <template>
-  <header>
+  <AppHeader />
+  <!-- <header>
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
 
   <RouterView />
 </template>
 
-<style>
-@import "@/assets/base.css";
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+}
+
+a {
+  text-decoration: none;
+}
+
+ul {
+  list-style-type: none;
+}
+
+button {
+  cursor: pointer;
+}
 </style>
