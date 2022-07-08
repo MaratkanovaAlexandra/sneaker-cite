1a7a5246d839dee1b1bc6274f06c7ccce6611164
Maratkanova Alexandra <alexandra.maratkanova@bravedevelopers.com>
2022-07-08 11:07:11 +0400

    add api

---
+++ src/api/index.ts
@@ -0,0 +1,36 @@
+import type { ProductType } from "@/types/Product";
+import axios from "axios";
+
+export const getProducts = async (
+  sort: string,
+  category = ""
+): Promise<ProductType[]> => {
+  return (
+    await axios.get(
+      `https://fakestoreapi.com/products${
+        category ? `/category/${category}` : ""
+      }`,
+      {
+        params: {
+          sort: sort,
+        },
+      }
+    )
+  ).data;
+};
+
+export const getCategories = async (): Promise<
+  { url: string; text: string }[]
+> => {
+  const result = await axios.get(
+    "https://fakestoreapi.com/products/categories"
+  );
+
+  return [
+    { url: "/", text: "New arrivals" },
+    ...result.data.map((i: string) => ({
+      url: `/?category=${i.replace(" ", "%20").replace("'", "%27")}`,
+      text: i,
+    })),
+  ];
+};
