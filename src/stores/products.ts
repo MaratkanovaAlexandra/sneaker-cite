9da4bf85a5bdce00e6eb03eaeae7da76370ff2c7
Maratkanova Alexandra <alexandra.maratkanova@bravedevelopers.com>
2022-07-08 11:30:37 +0400

    change store name

--- src/stores/products.ts
+++ src/stores/products.ts
@@ -1,5 +1,5 @@
 import type { ProductType } from "@/types/Product";
-import axios from "axios";
+import { getProducts } from "@/api";
 import { defineStore } from "pinia";
 
 export const useProductsStore = defineStore({
@@ -7,58 +7,51 @@ export const useProductsStore = defineStore({
   state: () => ({
     products: [] as ProductType[],
     sort: "asc",
-    maxRate: 5,
-    minRate: 0,
-    maxCount: 1000,
-    minCount: 1,
+    rate: { min: 0, max: 5 },
+    count: { min: 1, max: 1000 },
+    category: "",
   }),
   getters: {
     isRotating: (state) => state.sort === "desc",
   },
   actions: {
     toggleSort() {
-      this.products = [];
       this.sort = this.sort === "asc" ? "desc" : "asc";
       this.getProducts();
     },
 
     async getProducts() {
-      const result = await axios.get("https://fakestoreapi.com/products", {
-        params: {
-          sort: this.sort,
-        },
-      });
+      this.products = await getProducts(this.sort, this.category);
+      this.getRating();
+    },
 
-      this.products = result.data;
-      console.log(this.products.map((product) => product.rating.count));
-      this.getMinRating();
-      this.getMaxRating();
+    setCategory(value: string) {
+      this.category = value;
+      this.getProducts();
     },
 
     setMaxRate(value: number) {
-      this.maxRate = value;
+      this.rate.max = value;
     },
 
     setMinRate(value: number) {
-      this.minRate = value;
+      this.rate.min = value;
     },
 
     setMinCount(value: number) {
-      this.minCount = value;
+      this.count.min = value;
     },
 
     setMaxCount(value: number) {
-      this.maxCount = value;
+      this.count.max = value;
     },
 
-    getMaxRating() {
-      this.maxRate = Math.round(
+    getRating() {
+      this.rate.max = Math.round(
         Math.max(...this.products.map((product) => product.rating.rate))
       );
-    },
-
-    getMinRating() {
-      this.minRate = Math.round(
+      //округлять к меньшему значению
+      this.rate.min = Math.round(
         Math.min(...this.products.map((product) => product.rating.rate))
       );
     },
