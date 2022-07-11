081659401053a7cf8e09a641f22568293ff7c159
Maratkanova Alexandra <alexandra.maratkanova@bravedevelopers.com>
2022-07-11 15:03:29 +0400

    add loader

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 import ProductDetails from "../components/ProductDetails.vue";
+import AppLoader from "../components/AppLoader.vue";
 
 const route = useRoute();
 </script>
@@ -8,5 +9,6 @@ const route = useRoute();
 <template>
   <Suspense>
     <ProductDetails :productId="Number(route.query.productId)" />
+    <template #fallback> <AppLoader /></template>
   </Suspense>
 </template>
