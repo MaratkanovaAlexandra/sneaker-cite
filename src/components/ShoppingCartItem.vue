189c9de3144f059fd45b45e02123993aedead35d
Maratkanova Alexandra <alexandra.maratkanova@bravedevelopers.com>
2022-07-12 12:44:30 +0400

    add shopping cart item

---
+++ src/components/ShoppingCartItem.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import { useShoppingCardStore } from "@/stores/shoppingCart";
+import type { shoppingCartItemType } from "@/types/shoppindCartItem";
+import { storeToRefs } from "pinia";
+import { ref } from "vue";
+import ProductCounter from "./ProductCounter.vue";
+
+const store = useShoppingCardStore();
+const { getShortedName, getPriceForProducts } = storeToRefs(store);
+const { setAmount, removeFormShoppingCard } = store;
+
+const props = defineProps({
+  item: {
+    type: Object as () => shoppingCartItemType,
+    required: true,
+  },
+});
+
+const amount = ref(props.item.amount);
+</script>
+
+<template>
+  <div class="item">
+    <div class="item__data">
+      <div class="item__img_wrapper">
+        <img :src="item.product.image" :alt="item.product.title" />
+      </div>
+
+      <div class="item__title">
+        <h4>{{ getShortedName(item.product.id) }}</h4>
+        <p>$ {{ item.product.price }}</p>
+      </div>
+    </div>
+
+    <ProductCounter
+      v-model="amount"
+      @reach-zero="removeFormShoppingCard(item.product.id)"
+      @update:model-value="setAmount(item.product.id, amount)"
+    />
+
+    <h4 class="item__price">
+      ${{ (getPriceForProducts(item.product.id) as number).toFixed(2) }}
+    </h4>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  max-width: 880px;
+  height: 128px;
+  padding: 24px 32px;
+
+  background: rgba(0, 0, 0, 0.04);
+
+  &__data {
+    height: 100%;
+    display: flex;
+    width: 247px;
+    align-items: center;
+    gap: 24px;
+  }
+
+  &__img_wrapper {
+    height: 100%;
+    width: 98px;
+    background-color: #fff;
+    padding: 8px 12px;
+    border-radius: 8px;
+  }
+
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: scale-down;
+  }
+
+  &__title {
+    h4 {
+      font-weight: 700;
+      font-size: 1.125rem;
+      line-height: 1.625rem;
+    }
+
+    p {
+      font-weight: 400;
+      font-size: 1rem;
+      line-height: 1.5rem;
+
+      color: rgba(0, 0, 0, 0.65);
+    }
+  }
+
+  &__price {
+    font-weight: 700;
+    font-size: 1.125rem;
+    line-height: 1.625rem;
+    width: 70px;
+    text-align: right;
+  }
+}
+</style>
--- src/stores/shoppingCart.ts
+++ src/stores/shoppingCart.ts
@@ -2,22 +2,56 @@ import type { shoppingCartItemType } from "../types/shoppindCartItem";
 import { defineStore } from "pinia";
 import type { ProductType } from "@/types/Product";
 
+const saceToLocalStorage = (bougthProducts: shoppingCartItemType[]) =>
+  localStorage.setItem("shoppingCart", JSON.stringify(bougthProducts));
+
 export const useShoppingCardStore = defineStore({
   id: "shoppingCart",
   state: () => ({
     bougthProducts: [] as shoppingCartItemType[],
   }),
 
-  getters: {},
+  getters: {
+    findProduct: (state) => (id: number) =>
+      state.bougthProducts.find((i) => i.product.id === id),
+
+    getShortedName: (state) => (id: number) =>
+      state.bougthProducts
+        .find((i) => i.product.id === id)
+        ?.product.title.slice(0, 11) + "...",
+
+    getPriceForProducts: (state) => (id: number) => {
+      const result = state.bougthProducts.find((i) => i.product.id === id);
+      if (result) return +result.product.price * result.amount;
+    },
+  },
 
   actions: {
     addToShoppingCard(product: ProductType, amount: number) {
-      this.bougthProducts.push({
-        id: product.id,
-        amount: amount,
-        product: product,
-      });
+      const result = this.findProduct(product.id);
+      if (result) result.amount += amount;
+      else
+        this.bougthProducts.push({
+          id: product.id,
+          amount: amount,
+          product: product,
+        });
+      saceToLocalStorage(this.bougthProducts);
     },
-    // removeFormShoppingCard() {},
+
+    removeFormShoppingCard(id: number) {
+      this.bougthProducts = this.bougthProducts.filter((i) => i.id !== id);
+      saceToLocalStorage(this.bougthProducts);
+    },
+
+    setBougthProducts(products: shoppingCartItemType[]) {
+      this.bougthProducts = products;
+    },
+
+    setAmount(id: number, amount: number) {
+      const product = this.findProduct(id);
+      if (product) product.amount = amount;
+      saceToLocalStorage(this.bougthProducts);
+    },
   },
 });
